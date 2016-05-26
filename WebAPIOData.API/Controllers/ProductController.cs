57db038ff8ac1044f18d2b2190e8dd015a82c543
baegitmen <baegitmen@BILGEADAM-PC>
2016-05-26 20:26:35 +0300

    Add project files.

---
+++ WebAPIOData.API/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.OData.Query;
+using WebAPIOData.API.Models.ORM;
+
+namespace WebAPIOData.API.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+        public ProductController()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+        }
+
+        //[Queryable(AllowedQueryOptions =AllowedQueryOptions.Top)]
+        [Queryable]
+        public IQueryable<Product> GetAllProducts()
+        {
+            IQueryable<Product> plist = db.Products.AsQueryable();
+            return plist;
+
+            //return db.Products;
+        }
+    }
+}
---
+++ WebAPIOData.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPIOData.API.WebApiApplication" Language="C#" %>
